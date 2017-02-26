3694e60f3aa5ba3b6bd087dda909221a3b2a9eff
Sjur Waagbø <sjur.waagbo@gmail.com>
2017-02-27 00:47:59 +0100

    Implemented changes to allow for user registration

--- users/views.py
+++ users/views.py
@@ -4,17 +4,18 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
 
 from .serializers import UserSerializer, UserCreateSerializer
 
 
-# Create your views here.
 
 class UserListCreate(APIView):
     """
     Create user view
     """
-    permission_classes = (IsAuthenticated, )
+    permission_classes = [permissions.AllowAny]
+
 
     def post(self, request):
         serializer = UserCreateSerializer(data=request.data)
@@ -22,14 +23,3 @@ class UserListCreate(APIView):
         user = serializer.save()
         data = UserSerializer(user).data
         return Response(data, status=status.HTTP_201_CREATED)
-
-
-@api_view(['GET'])
-@permission_classes([permissions.IsAuthenticated, ])
-def get_current_user(request):
-    """
-    Returns the logged in user instance
-    """
-    user = request.user
-    serializer = UserSerializer(user)
-    return Response(serializer.data)
