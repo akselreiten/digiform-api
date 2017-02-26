ac7da4aaf5e1d5e94d20c91ac763f6b889afcac4
Sjur Waagbø <sjur.waagbo@gmail.com>
2017-02-26 20:07:32 +0100

    Did changes to allow for creating user

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
+from .views import UserListCreate
+
 urlpatterns = [
     url(r'^api-token-auth/$',
         obtain_jwt_token,
@@ -10,5 +12,10 @@ urlpatterns = [
     url(r'^api-token-verify/$',
         verify_jwt_token,
         name='verify-token'),
+
+    url(r'^createUser/$',
+        UserListCreate.as_view(),
+        name='current-user'),
+
 ]
 
