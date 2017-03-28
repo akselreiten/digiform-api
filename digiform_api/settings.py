e1f83b6d556aa2570b1a322c0c096515d2fe7b1f
aksel.reiten <aksel.reiten@gmail.com>
2017-03-28 21:42:24 +0200

    Removed reference to static dir

--- digiform_api/settings.py
+++ digiform_api/settings.py
@@ -159,9 +159,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = ()
 
 
 # Simplified static file serving.
