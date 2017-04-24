aed7b0565eaadaf0a3d67c7c02edfb42637a2a7e
aksel.reiten <aksel.reiten@gmail.com>
2017-04-24 22:07:35 +0200

    Added ulrs and views

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
-from .views import UserListCreate
+from .views import UserListCreate, UserHandling
 
 urlpatterns = [
     url(r'^api-token-auth/$',
@@ -16,6 +16,14 @@ urlpatterns = [
     url(r'^createUser/$',
         UserListCreate.as_view(),
         name='current-user'),
+
+    url(r'^getUser/$',
+        UserListCreate.as_view(),
+        name='get-user'),
+
+    url(r'^logout/$',
+        UserHandling.as_view(),
+        name='logout'),
 
 ]
 
--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
+from django.contrib.auth import logout
 
 from .serializers import UserSerializer, UserCreateSerializer
 
@@ -16,10 +17,25 @@ class UserListCreate(APIView):
     """
     permission_classes = [permissions.AllowAny]
 
-
     def post(self, request):
         serializer = UserCreateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         data = UserSerializer(user).data
         return Response(data, status=status.HTTP_201_CREATED)
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserHandling(APIView):
+    """
+    Handles users
+    """
+
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        logout(request)
+        return Response(data=None, status=status.HTTP_200_OK)
